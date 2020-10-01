e211ca303f39bb69f57c4c886caea7951fe5b194
mausam1112 <40898851+mausam1112@users.noreply.github.com>
2020-10-01 14:13:16 +0545

    Update speech_to_text.py
    Added model beam width, Scorer language model weight and word insertion weight

--- speech_to_text.py
+++ speech_to_text.py
@@ -37,13 +37,22 @@ def record_voice_stream(stream_file_name):
 
 
 def predict_speech_to_text(stream_file):
+    alpha = 0.85
+    beta = 1.75
+    beam_width = 500
+    
     # Initialize the model
     speech_model = Model(MODEL_PATH)
 
     # Enable language scorer to improve the accuracy
     speech_model.enableExternalScorer(SCORER_PATH)
-    # You can play with setting the model Beam Width, Scorer language model weight and word insertion weight
-
+    
+    #set beam width. A larger beam width value generates better results at the cost of decoding time.
+    speech_model.setBeamWidth(beam_width)
+    
+    # setting the Scorer language model weight (alpha) and word insertion weight(beta)
+    speech_model.setScorerAlphaBeta(alpha, beta)
+    
     # Use scipy to covert wav file into numpy array
     _, audio = wav.read(stream_file)
     return speech_model.stt(audio)
